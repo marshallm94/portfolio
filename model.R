94b6542df91f7f8a37e6f9349f6f591c6014ceb6
Marshall McQuillen <marshallm94@gmail.com>
2017-11-22 07:20:54 -0700

    Filter non-english words

--- model.R
+++ model.R
@@ -1,16 +1,48 @@
 source("/Users/marsh/data_science_coursera/JHU_capstone/setup.R")
 source("/Users/marsh/data_science_coursera/JHU_capstone/eda.R")
 
-# from eda.R
-x <- total_trigram
-df <- x
+# recall variables from setup.R
+total_word
+total_bigram
+total_trigram
+
+# remove non-english words from total_word
 english <- NULL
 for(i in letters) {
-    letter <- grep(i, df[[2]])
+    letter <- grep(i, total_word[[2]])
     english <- c(english, letter)
 }
 english <- unique(english)
-df <- df[english,]
+word_df <- total_word[english,]
+
+# remove non-english words from total_bigram
+english <- NULL
+for(i in letters) {
+    letter <- grep(i, total_bigram[[2]])
+    english <- c(english, letter)
+}
+english <- unique(english)
+bigram_df <- total_bigram[english,]
+
+# remove non-english words from total_trigram
+english <- NULL
+for(i in letters) {
+    letter <- grep(i, total_trigram[[2]])
+    english <- c(english, letter)
+}
+english <- unique(english)
+trigram_df <- total_trigram[english,]
+
+# enter sentence as y
+a <- "The guy in front of me just bought a pound of bacon, a bouquet, and a case of"
+
+# 1. reduce sentence to last 3 - 4 words
+fragment <- grep(" [a-zA-Z] [a-zA-Z] [a-zA-Z]", a)
+selected <- grep(a, df$ngram)
+selected_df <- df[selected,]
+
+
+
 df_dist <- count(df, ngram, sort = TRUE)
 topx <- df_dist[1:10,]
 
@@ -18,5 +50,6 @@ topx <- df_dist[1:10,]
 # entire sentence/sequence and add to file (add to data frame). For future calls
 # to function, give these user-inputed sequences for weight
 
-y <- "to go"
-grep(x, df_dist$ngram)
+
+# beta area
+
