c1816a26ad47b5789ac8e5011bed3ceb4d06b395
Marshall McQuillen <marshallm94@gmail.com>
2017-12-07 07:22:39 -0700

    rm sentence_to_word and sentence_to_ngram fx's: replace with tokenize_test fx

--- model.R
+++ model.R
@@ -1,92 +1,89 @@
+suppressPackageStartupMessages(library(tidyverse))
+suppressPackageStartupMessages(library(quanteda))
+suppressPackageStartupMessages(library(data.table))
+suppressPackageStartupMessages(library(sqldf))
+
 setwd("/Users/marsh/data_science_coursera/JHU_capstone")
 
-clean_waste <- function(x) {
-    y <- quantile(x[[2]], probs = 0.75)
+clean_waste <- function(x, n) {
     name <- deparse(substitute(x))
-    message(paste("Removing observations with counts <", y, "from", name, sep = " "))
-    z <- subset(x, x[[2]] >= y)
+    message(paste("Removing observations with counts <", n, "from", name, sep = " "))
+    z <- subset(x, x[[2]] >= n)
     rm(list = name, pos = ".GlobalEnv")
     message(paste(name, "removed from global environment"))
     z
 }
 
-# clean files
+# clean unnecessary ngrams in files
 unigram <- readRDS("./ngrams/unigram.rds")
-clean_unigram <- clean_waste(unigram)
+clean_unigram <- clean_waste(unigram, 5)
 saveRDS(clean_unigram, "./ngrams/clean_unigram.rds")
 rm(clean_unigram)
 
 bigram <- readRDS("./ngrams/bigram.rds")
-clean_bigram <- clean_waste(bigram)
+clean_bigram <- clean_waste(bigram, 5)
 saveRDS(clean_bigram, "./ngrams/clean_bigram.rds")
 rm(clean_bigram)
 
 trigram <- readRDS("./ngrams/trigram.rds")
-clean_trigram <- clean_waste(trigram)
+clean_trigram <- clean_waste(trigram, 5)
 saveRDS(clean_trigram, "./ngrams/clean_trigram.rds")
 rm(clean_trigram)
 
 quadgram <- readRDS("./ngrams/quadgram.rds")
-clean_quadgram <- clean_waste(quadgram)
+clean_quadgram <- clean_waste(quadgram, 5)
 saveRDS(clean_quadgram, "./ngrams/clean_quadgram.rds")
 rm(clean_quadgram)
 
 quintgram <- readRDS("./ngrams/quintgram.rds")
-clean_quintgram <- clean_waste(quintgram)
+clean_quintgram <- clean_waste(quintgram, 5)
 saveRDS(clean_quintgram, "./ngrams/clean_quintgram.rds")
 rm(clean_quintgram)
 
 sextagram <- readRDS("./ngrams/sextagram.rds")
-clean_sextagram <- clean_waste(sextagram)
+clean_sextagram <- clean_waste(sextagram, 5)
 saveRDS(clean_sextagram, "./ngrams/clean_sextagram.rds")
 rm(clean_sextagram)
 
+unigram <- readRDS("./ngrams/clean_unigram.rds")
+bigram <- readRDS("./ngrams/clean_bigram.rds")
+trigram <- readRDS("./ngrams/clean_trigram.rds")
+quadgram <- readRDS("./ngrams/clean_quadgram.rds")
+quintgram <- readRDS("./ngrams/clean_quintgram.rds")
+sextagram <- readRDS("./ngrams/clean_sextagram.rds")
+
 add_tf <- function(x) {
-    y <- sum(x$count)
+    y <- sum(x[[2]])
     x <- mutate(x, term_freq = count/y)
     as.data.table(x)
 }
 
-tf_unigram <- add_tf(unigram)
-ordered_bigram <- add_tf(ordered_bigram)
-ordered_trigram <- add_tf(ordered_trigram)
-ordered_quadgram <- add_tf(ordered_quadgram)
-ordered_quintgram <- add_tf(ordered_quintgram)
-
-# in order to improve performance, will reduce the data sets to only those 
-# tokens/ngrams that have a minimum count. (Since the count/term-frequency is so
-# low, the probability of my model choosing those ngrams is rather low, therefore
-# keeping them in the data set is a waste of memory)
-
-ordered_word <- subset(tf_unigram, count >= quantile(tf_unigram$count, probs = 0.75))
-ordered_bigram <- subset(ordered_bigram, n >= quantile(ordered_bigram$n, probs = 0.90))
-ordered_trigram <- subset(ordered_trigram, n >= quantile(ordered_trigram$n, probs = 0.90))
-ordered_quadgram <- subset(ordered_quadgram, n >= quantile(ordered_quadgram$n, probs = 0.90))
-ordered_quintgram <- subset(ordered_quintgram, n >= quantile(ordered_quintgram$n, probs = 0.90))
-
-sentence_to_word <- function(x) {
-    df <- as_data_frame(x)
-    df2 <- mutate(df, line = rownames(df))
-    df2$line <- as.integer(df2$line)
-    df2 <- select(df2, line, value)
-    df2 <- rename(df2, text = value)
-    df3 <- df2 %>% unnest_tokens(output = word, text, token = 'words')
-    df3
-}
-
-sentence_to_ngram <- function(x, n) {
-    df <- as_data_frame(x)
-    df2 <- mutate(df, line = rownames(df))
-    df2$line <- as.integer(df2$line)
-    df2 <- select(df2, line, value)
-    df2 <- rename(df2, text = value)
-    df3 <- df2 %>% unnest_tokens(output = ngram, text, token = 'ngrams', n = n)
-    df3
-}
+unigram <- add_tf(unigram)
+bigram <- add_tf(bigram)
+trigram <- add_tf(trigram)
+quadgram <- add_tf(quadgram)
+quintgram <- add_tf(quintgram)
+sextagram <- add_tf(sextagram)
 
 ######################### ENTER TEST SENTENCE/SEQUENCE HERE
 a <- "You're the reason why I smile everyday. Can you follow me please? It would mean the"
 #########################
+
+tokenize_test <- function(x, n) {
+    x <- a
+    n <- 2
+    tok <- tokens(a,
+                  remove_numbers = TRUE,
+                  remove_punct = TRUE,
+                  remove_symbols = TRUE,
+                  remove_hyphens = TRUE,
+                  remove_url = TRUE)
+    tokens <- tokens_ngrams(tok, n, concatenator = " ")
+    unlisted <- unlist(tokens)
+    search_for <- unname(tail(unlisted, n = 1))
+    search_for
+}
+
 
 word_test <- sentence_to_word(a)
 bigram_test <- sentence_to_ngram(a, n = 2)
